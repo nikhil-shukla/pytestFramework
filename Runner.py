4d9a111140629797da5c662d91af9eb71be25fba
nikhil shukla <nikhil@nikhils-MacBook.local>
2024-01-17 21:37:54 +0530

    new changes

--- Runner.py
+++ Runner.py
@@ -1,26 +1,29 @@
 import subprocess
 from argparse import ArgumentParser
-
 from tests.BaseTest import BaseTest
 
 
 def parse_args():
     parser = ArgumentParser(description="Test Runner")
-
-    parser.add_argument('-v', '--verbose', default='True', help='Enable Verbose')
-    parser.add_argument('-r', '--report', default='True', help='Report Generation')
-    parser.add_argument('-n', '--parallel', default='True', help='For parallel execution')
+    parser.add_argument("-b", "--browser", default="chrome", help="Choose Browser")
+    parser.add_argument("-v", "--verbose", default="True", help="Enable Verbose")
+    parser.add_argument("-r", "--report", default="True", help="Report Generation")
+    parser.add_argument("-n", "--parallel", default="True", help="For parallel execution")
     return parser.parse_args()
 
 
 def run_tests(args):
     base = BaseTest()
     pytest_command = ["pytest"]
+
+    if args.browser:
+        pytest_command.append('--browser=chrome')
+
     if args.verbose:
         pytest_command.append("-v")
 
     if args.report:
-        report_name = base.generate_timestamp() + '_report'
+        report_name = base.generate_timestamp() + "_report"
         pytest_command.append(f"--html=reports/{report_name}.html")
 
     if args.parallel:
